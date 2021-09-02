144c08def5f10761b4f0e1f7bca0d547715e1781
Tanimowo Emmanuel <oluwasegunprosperity@gmail.com>
2021-09-02 18:51:59 +0100

    fix

--- users/admin.py
+++ users/admin.py
@@ -9,5 +9,7 @@ class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
+    list_display = ['email', 'username', 'age', 'is_staff']
+
 
 admin.site.register(CustomUser, CustomUserAdmin)
