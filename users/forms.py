828a977bfb530f36bfbcd869e7ebd66d805a4d13
mannuel25 <oluwasegunprosperity@gmail.com>
2021-09-04 11:34:33 +0100

    add

--- users/forms.py
+++ users/forms.py
@@ -7,10 +7,11 @@ class CustomUserCreationForm(UserCreationForm):
 
     class Meta(UserCreationForm):
         model = CustomUser
-        fields = UserCreationForm.Meta.fields + ('age',)
+        fields = ('username','email','age',)
 
 class CustomUserChangeForm(UserChangeForm):
     
     class Meta:
         model = CustomUser
-        fields = UserChangeForm.Meta.fields
+        fields = ('username','email','age',)
+       
