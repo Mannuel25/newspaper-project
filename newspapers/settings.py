054b8f684fda5fe762d0e4715b03fd223cbc73ec
mannuel25 <oluwasegunprosperity@gmail.com>
2021-09-04 15:18:09 +0100

    added password reset feature

--- newspapers/settings.py
+++ newspapers/settings.py
@@ -137,3 +137,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
