a96a421e0d470a0e8dbec538033a6159b4307711
mannuel25 <oluwasegunprosperity@gmail.com>
2021-09-05 17:25:21 +0100

    add

--- users/views.py
+++ users/views.py
@@ -1,5 +1,8 @@
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+from django.conf import settings
+from django.core.mail import send_mail
+
 
 from .forms import CustomUserCreationForm
 
