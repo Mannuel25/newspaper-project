14bd9295bc35f74bb0127ac9a502c065f1d96f8f
mannuel25 <oluwasegunprosperity@gmail.com>
2021-09-05 17:09:52 +0100

    add

--- newspapers/settings.py
+++ newspapers/settings.py
@@ -124,6 +124,13 @@ USE_L10N = True
 
 USE_TZ = True
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = #sender's email-id
+EMAIL_HOST_PASSWORD = #password associated with above email-id
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
