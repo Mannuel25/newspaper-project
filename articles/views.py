efbe9cc4b8b3cdb5cc0616658afc416394dcb62a
mannuel25 <oluwasegunprosperity@gmail.com>
2021-09-06 13:45:04 +0100

    update

--- articles/views.py
+++ articles/views.py
@@ -29,4 +29,8 @@ class ArticleDeleteView(DeleteView):
 class ArticleCreateView(CreateView):
     model = Article
     template_name = 'article_new.html'
-    fields = ('title', 'body', 'author')
+    fields = ('title', 'body')
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        retrun super().form_valid(form)
