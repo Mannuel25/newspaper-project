a7f54ebf7f6218a1b67ecd3b456daab4209dad89
mannuel25 <oluwasegunprosperity@gmail.com>
2021-09-06 14:30:31 +0100

    added authorizations

--- articles/views.py
+++ articles/views.py
@@ -1,7 +1,12 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import (
+    LoginRequiredMixin,
+    UserPassesTestMixin,
+)
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import (
-    UpdateView, DeleteView, CreateView
+    UpdateView,
+    DeleteView, 
+    CreateView,
 )
 from django.urls import reverse_lazy
 
@@ -21,19 +26,26 @@ class ArticleDetailView(LoginRequiredMixin, DetailView):
     login_url = 'login'
 
 
-class ArticleUpdateView(LoginRequiredMixin, UpdateView):
+class ArticleUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Article
     fields = ('title', 'body',)
     template_name = 'article_edit.html'
     login_url = 'login'
 
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
 
-class ArticleDeleteView(LoginRequiredMixin, DeleteView):
+
+class ArticleDeleteView(LoginRequiredMixin,UserPassesTestMixin, DeleteView):
     model = Article
     template_name = 'article_delete.html'
     success_url = reverse_lazy('article_list')
     login_url = 'login'
 
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
 
 class ArticleCreateView(LoginRequiredMixin, CreateView):
     model = Article
